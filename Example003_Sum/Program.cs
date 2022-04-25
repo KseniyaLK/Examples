8de2f2c0eaa77d3f5bc833150e4382e7779d275a
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-04-25 08:10:13 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberА = 3;
-int numberB = 10;
+﻿int numberА = new Random().Next(1, 10);
+Console.WriteLine(numberА);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberА + numberB;
 Console.WriteLine(result);
 
